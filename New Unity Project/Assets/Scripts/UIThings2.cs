7345ec722934a89e6da54833c1391ff494b4e34a
colorplease <alistairvtran@gmail.com>
2021-06-28 09:49:14 -0700

    massive UI Improvements

---
+++ New Unity Project/Assets/Scripts/UIThings2.cs
@@ -0,0 +1,80 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIThings2 : MonoBehaviour
+{
+    public Image image;
+    public Image image2;
+    public Image image3;
+    public Image image4;
+    public Image image5;
+    public int blockType;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        image.color = new Color(image.color.r, image.color.g, image.color.b, 0f);
+        image2.color = new Color(image.color.r, image.color.g, image.color.b, 0f);
+        image3.color = new Color(image.color.r, image.color.g, image.color.b, 0f);
+        image4.color = new Color(image.color.r, image.color.g, image.color.b, 0f);
+        image5.color = new Color(image.color.r, image.color.g, image.color.b, 0f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+//Block Types
+    public void one()
+    {
+        image.color = new Color(image.color.r, image.color.g, image.color.b, 0.36f);
+        image2.color = new Color(image.color.r, image.color.g, image.color.b, 0f);
+        image3.color = new Color(image.color.r, image.color.g, image.color.b, 0f);
+        image4.color = new Color(image.color.r, image.color.g, image.color.b, 0f);
+        image5.color = new Color(image.color.r, image.color.g, image.color.b, 0f);
+        blockType = 1;
+    }
+
+    public void two()
+    {
+        image2.color = new Color(image.color.r, image.color.g, image.color.b, 0.36f);
+         image.color = new Color(image.color.r, image.color.g, image.color.b, 0f);
+        image3.color = new Color(image.color.r, image.color.g, image.color.b, 0f);
+        image4.color = new Color(image.color.r, image.color.g, image.color.b, 0f);
+        image5.color = new Color(image.color.r, image.color.g, image.color.b, 0f);
+        blockType = 2;
+    }
+
+    public void three()
+    {
+        image3.color = new Color(image.color.r, image.color.g, image.color.b, 0.36f);
+         image.color = new Color(image.color.r, image.color.g, image.color.b, 0f);
+        image2.color = new Color(image.color.r, image.color.g, image.color.b, 0f);
+        image4.color = new Color(image.color.r, image.color.g, image.color.b, 0f);
+        image5.color = new Color(image.color.r, image.color.g, image.color.b, 0f);
+        blockType = 3;
+    }
+
+    public void four()
+    {
+        image4.color = new Color(image.color.r, image.color.g, image.color.b, 0.36f);
+         image.color = new Color(image.color.r, image.color.g, image.color.b, 0f);
+        image2.color = new Color(image.color.r, image.color.g, image.color.b, 0f);
+        image3.color = new Color(image.color.r, image.color.g, image.color.b, 0f);
+        image5.color = new Color(image.color.r, image.color.g, image.color.b, 0f);
+        blockType = 4;
+    }
+
+    public void five()
+    {
+        image5.color = new Color(image.color.r, image.color.g, image.color.b, 0.36f);
+         image.color = new Color(image.color.r, image.color.g, image.color.b, 0f);
+        image2.color = new Color(image.color.r, image.color.g, image.color.b, 0f);
+        image3.color = new Color(image.color.r, image.color.g, image.color.b, 0f);
+        image4.color = new Color(image.color.r, image.color.g, image.color.b, 0f);
+        blockType = 5;
+    }
+}
